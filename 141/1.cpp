7c6abba5960083e82d7b7799ff217ecea8810454
yipple <1275432232@qq.com>
2020-02-28 21:13:53 +0800

    2020.2.28

---
+++ 141/1.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        set<ListNode *> nodes;
+
+        while (head != NULL) {
+            if (nodes.find(head) == nodes.end())
+                nodes.insert(head);
+            else
+                return true;
+
+            head = head->next;
+        }
+
+        return false;
+    }
+};
