12b37676a3337a74561933c306d9ac2f37775806
yipple <1275432232@qq.com>
2020-03-17 18:19:55 +0800

    2020.3.17

--- 1160/1.cpp
+++ 1160/1.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int countCharacters(vector<string> &words, string chars) {
-        int chars_hash[26]{0};
-        for (char ch : chars)
-            ++chars_hash[ch - 'a'];
+        unordered_map<char, int> chars_map;
+        for (int i = 0; i < chars.size(); ++i)
+            if (chars_map.find(chars[i]) != chars_map.end())
+                chars_map[chars[i]] += 1;
+            else
+                chars_map[chars[i]] = 1;
 
         int count = 0;
         for (string word : words) {
-            int word_hash[26]{0};
-            for (char ch : word)
-                ++word_hash[ch - 'a'];
+            unordered_map<char, int> word_map;
+            for (int i = 0; i < word.size(); ++i)
+                if (word_map.find(word[i]) != word_map.end())
+                    word_map[word[i]] += 1;
+                else
+                    word_map[word[i]] = 1;
 
-            bool flag = true;
-            for (int i = 0; i < 26; ++i)
-                if (chars_hash[i] < word_hash[i])
-                    flag = false;
-
-            if (flag)
-                count += word.size();
+            count += word.size();
+            for (unordered_map<char, int>::iterator iter = word_map.begin(); iter != word_map.end(); ++iter) {
+                if ((chars_map.find(iter->first) == chars_map.end()) || chars_map[iter->first] < iter->second) {
+                    count -= word.size();
+                    break;
+                }
+            }
         }
 
         return count;
