1c37f7e6f24200f53199453e5cda3980c156b3ad
yipple <1275432232@qq.com>
2020-03-20 00:08:25 +0800

    2020.3.20

--- 409/1.cpp
+++ 409/1.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char, int> count;
-        int ans = 0;
-        for (char c : s)
-            ++count[c];
-        for (auto p : count) {
-            int v = p.second;
-            ans += v / 2 * 2;
-            if (v % 2 == 1 and ans % 2 == 0)
-                ++ans;
+        unordered_map<char, int> char_map;
+        int max_length = 0;
+
+        for (char ch : s)
+            ++char_map[ch];
+
+        for (auto p : char_map) {
+            int count = p.second;
+            max_length += count / 2 * 2;
+            if (count % 2 == 1 and max_length % 2 == 0)
+                ++max_length;
         }
-        return ans;
+
+        return max_length;
     }
 };
