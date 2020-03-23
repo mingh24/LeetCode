fb36b01fa8a1b72cb4edcce16d0b6f49f6761c47
yipple <1275432232@qq.com>
2020-03-23 23:38:40 +0800

    2020.3.23

--- 945/1.cpp
+++ 945/1.cpp
@@ -6,9 +6,11 @@ public:
         int move = 0;
         for (int i = 1; i < A.size(); ++i) {
             if (A[i] <= A[i - 1]) {
-                move += (A[i] + 1 - A[i - 1]);
+                move += (A[i - 1] + 1 - A[i]);
                 A[i] = A[i - 1] + 1;
             }
         }
+
+        return move;
     }
 };
