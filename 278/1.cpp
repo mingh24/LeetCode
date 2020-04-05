3ee4265b344ef128f8671f17f8ef0644c472d173
yipple <1275432232@qq.com>
2020-04-05 16:52:40 +0800

    2020.4.5

---
+++ 278/1.cpp
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+
+            if (isBadVersion(middle))
+                right = middle;
+            else
+                left = middle + 1;
+        }
+
+        return right;
+    }
+};
