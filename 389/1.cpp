264e1c69bd74223d92d62a869dc6f859080f4342
yipple <1275432232@qq.com>
2020-04-24 23:36:42 +0800

    2020.4.24

---
+++ 389/1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+
+        for (char ch : t)
+            sum += ch;
+
+        for (char ch : s)
+            sum -= ch;
+
+        return sum;
+    }
+};
