e4d4428f40fd20fcb71f3671ef1d209c5a4f7e2c
yipple <1275432232@qq.com>
2020-04-29 23:53:47 +0800

    2020.4.29

--- 589/1.cpp
+++ 589/1.cpp
@@ -1,22 +1,22 @@
 /*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
+    // Definition for a Node.
+    class Node {
+    public:
+        int val;
+        vector<Node*> children;
 
-    Node() {}
+        Node() {}
 
-    Node(int _val) {
-        val = _val;
-    }
+        Node(int _val) {
+            val = _val;
+        }
 
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
+        Node(int _val, vector<Node*> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+    */
 
 class Solution {
 public:
