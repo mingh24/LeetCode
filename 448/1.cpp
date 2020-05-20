851907683433b63f3b0ba23c18b651395261985f
yipple <1275432232@qq.com>
2020-05-20 23:55:28 +0800

    2020.5.20

---
+++ 448/1.cpp
@@ -0,0 +1,24 @@
+static const auto io_sync_off = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[abs(nums[i]) - 1] > 0)
+                nums[abs(nums[i]) - 1] *= -1;
+        }
+
+        vector<int> answer;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                answer.emplace_back(i + 1);
+            }
+        }
+
+        return nums;
+    }
+};
