57aa7d50b1fcab366cff806f73deadb3c69ec8a5
yipple <1275432232@qq.com>
2020-06-26 20:55:59 +0800

    2020.6.26

---
+++ 剑指 Offer 22. 链表中倒数第k个节点/2.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getKthFromEnd(ListNode *head, int k) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while (fast and k--) {
+            fast = fast->next;
+        }
+
+        while (fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        return slow;
+    }
+};
