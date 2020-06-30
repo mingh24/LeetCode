29fa864af999ad0be83394789a0e667d527b4ec4
yipple <1275432232@qq.com>
2020-07-01 00:07:33 +0800

    2020.7.1

--- 剑指 Offer 53 - II. 0～n-1中缺失的数字/1.cpp
+++ 剑指 Offer 53 - II. 0～n-1中缺失的数字/1.cpp
@@ -1,3 +1,9 @@
+static const auto io_sync_off = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
     int missingNumber(vector<int> &nums) {
