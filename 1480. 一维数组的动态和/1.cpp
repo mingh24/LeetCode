4fb5248d766688392ea2b5ca300ce83fcb6cca63
yipple <1275432232@qq.com>
2020-07-09 22:21:53 +0800

    2020.7.9

---
+++ 1480. 一维数组的动态和/1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int> &nums) {
+        vector<int> answer(nums.size());
+        answer[0] = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            answer[i] = answer[i - 1] + nums[i];
+        }
+
+        return answer;
+    }
+};
