76bb7806f5cc671bf134498b97752586bc09dc13
yipple <1275432232@qq.com>
2020-07-17 15:06:27 +0800

    2020.7.17

--- 1436/1.cpp
+++ 1436/1.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
-    string destCity(vector<vector<string>>& paths) {
-        string answer = paths[0][1];
-        
+    string destCity(vector<vector<string>> &paths) {
+        unordered_map<string, string> paths_map;
+
+        for (auto path : paths) {
+            paths_map[path[0]] = path[1];
+        }
+
+        for (auto path : paths) {
+            if (paths_map.find(path[1]) == paths_map.end()) {
+                return path[1];
+            }
+        }
+
+        return "";
     }
 };
