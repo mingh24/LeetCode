1f63a6110af7f9b4c594a15e3ca7a30c773c6587
yipple <1275432232@qq.com>
2020-08-01 23:52:58 +0800

    2020.8.1

---
+++ 232. 用栈实现队列/1.cpp
@@ -0,0 +1,48 @@
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        s1.emplace(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int front = peek();
+        s2.pop();
+        return front;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                int temp = s1.top();
+                s1.pop();
+                s2.emplace(temp);
+            }
+        }
+
+        return s2.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return s1.empty() and s2.empty();
+    }
+
+private:
+    stack<int> s1, s2;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
