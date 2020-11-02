777d897a645240afae58c7249a6da59e9f0cf84f
yipple <1275432232@qq.com>
2020-11-02 23:59:37 +0800

    2020.11.2

---
+++ 剑指 Offer 24. 反转链表/2.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverseList(ListNode *head) {
+        if (!head || !head->next) {
+            return head;
+        }
+
+        ListNode *next = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return next;
+    }
+};
